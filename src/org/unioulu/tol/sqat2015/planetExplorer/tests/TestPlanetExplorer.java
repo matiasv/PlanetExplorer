fa11b49cf10bd7080a1ddd81b43c5e1fba30690a
somename <someemail>
2015-10-23 13:28:56 +0300

    besouro automatic message

--- src/org/unioulu/tol/sqat2015/planetExplorer/tests/TestPlanetExplorer.java
+++ src/org/unioulu/tol/sqat2015/planetExplorer/tests/TestPlanetExplorer.java
@@ -65,7 +65,7 @@ public class TestPlanetExplorer {
 		
 		//the same thing moving across the grid to the top
 		//note: also changing direction here (to South)
-		String cmd ="rrff";
+		String cmd ="rrfff";
 		res = explorer.executeCommand(cmd);
 		assertEquals(res,"(0,0,S)");
 		
