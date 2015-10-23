a6951fdf3c1a0a6d5e63d61feff79d4cd9abe23c
somename <someemail>
2015-10-23 11:37:20 +0300

    besouro automatic message

--- src/org/unioulu/tol/sqat2015/planetExplorer/PlanetExplorer.java
+++ src/org/unioulu/tol/sqat2015/planetExplorer/PlanetExplorer.java
@@ -4,6 +4,10 @@ package org.unioulu.tol.sqat2015.planetExplorer;
 // ID: 2158866
 // Finish time:
 public class PlanetExplorer {
+	public enum Facing {
+		
+	}
+	
 	public PlanetExplorer(int x, int y, String obstacles){
 	/*	x and y represent the size of the grid.
 	 *  Obstacles is a String formatted as follows: "(obs1_x,obs1_y)(obs2_x,obs2_y)...(obsN_x,obsN_y)" with no white spaces. 
